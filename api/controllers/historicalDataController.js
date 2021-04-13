14ad867fdd13f8dc48538f87705aa3ec4829d9df
viniciuslazzari <vinilazzari028@gmail.com>
2021-04-13 19:49:09 -0300

    ✨ Add API parameters

--- api/controllers/historicalDataController.js
+++ api/controllers/historicalDataController.js
@@ -4,7 +4,9 @@ module.exports = () => {
 	const controller = {};
 
 	controller.getHistoricalData = (req, res) => {
-		var process = spawn('python', ['./microservices/bitcoinPriceHistory.py']);
+		var process = spawn('python', ['./microservices/bitcoinPriceHistory.py',
+			req.query.coin,
+			req.query.timePeriod]);
 
 		process.stdout.on('data', function (data) {
 			res.status(200).send(JSON.parse(data));
--- api/microservices/bitcoinPriceHistory.py
+++ api/microservices/bitcoinPriceHistory.py
@@ -1,23 +1,35 @@
 import requests
 import pandas as pd
 import sys
+import os
+import json
+from dotenv import load_dotenv
 
-url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
-# url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2020-09-05'
+load_dotenv()
+url = 'https://api.coinranking.com/v2/coin/'
+
+
+def jsonToDataframe(json):
+    df = pd.DataFrame(columns=['price'])
+
+    for argument in json:
+        df.loc[argument['timestamp']] = round(float(argument['price']), 2)
+
+    return df
 
 
 def getBitcoinPriceHistory():
     try:
-        response = requests.get(url)
+        requestUrl = url + sys.argv[1] + '/history?timePeriod=' + sys.argv[2]
+        response = requests.get(
+            requestUrl, headers={"x-access-token": os.getenv("API_KEY")})
     except requests.exceptions.RequestException as err:
         print('Something went wrong: ' + err)
         sys.stdout.flush()
     else:
         if response.status_code == 200:
-            pricesHistory = response.json()['bpi']
-            df = pd.DataFrame.from_dict(
-                pricesHistory, orient='index', columns=['Price'])
-            df['Price'] = round(df['Price'], 2)
+            pricesHistory = response.json()['data']['history']
+            df = jsonToDataframe(pricesHistory)
             output = df.to_json()
             print(output)
             sys.stdout.flush()
